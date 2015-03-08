edd1b508933c6f69ac4385710c9e201030425cad
Martin Holm Jensen <martin.holm.j@gmail.com>
2015-03-08 15:21:47 +0100

    Update 2015-03-08T14:21:46.861Z

--- app.js
+++ app.js
@@ -88,67 +88,6 @@
 (function () {
 	'use strict';
 
-	angular.module('playground.area', [ 'playground.toolbar', 'playground.browser', 'playground.transformation', 'playground.paper' ])
-
-	// TODO Show active (last clicked) widget using panel-primary and panel-info
-
-	// TODO snap areas to percentages using relative coords
-	// TODO dragable (or lockable) on/off in panel top
-
-	.directive('wooArea', ['$document', function( $document ) {
-		return {
-			restrict: 'EA',
-			scope: {
-				area: '=',
-				name: '@',
-				panelType: '@',
-				layout: '=',
-				browser: '=',
-				currentFile: '=',
-				woo: '@'
-			},
-			templateUrl: 'app/content/playground/area.tpl.html',
-			link: function(scope) {
-				var startX = 0, startY = 0, x = 0, y = 0;
-
-
-				scope.dragStart = function( event ) {
-					// Prevent default dragging of selected content
-					event.preventDefault();
-					x = scope.layout.style.left;
-					y = scope.layout.style.top;
-					startX = event.screenX - x;
-					startY = event.screenY - y;
-					$document.on('mousemove', scope.beingDragged);
-					$document.on('mouseup', scope.dragEnd);
-				};
-
-				scope.beingDragged = function() {
-					y = event.screenY - startY;
-					x = event.screenX - startX;
-
-					scope.$apply( function() {
-						scope.layout.style = {
-							top: y,
-							left: x
-						};
-					});
-				};
-
-				scope.dragEnd = function () {
-					$document.off('mousemove', scope.beingDragged);
-					$document.off('mouseup', scope.dragEnd);
-				};
-			}
-		};
-	}])
-	;
-
-}());
-
-(function () {
-	'use strict';
-
 	angular.module('playground.config', [])
 
 	;
@@ -285,7 +224,9 @@ Salg';
 						layout: {
 							style: {
 								left: 740,
-								top: 15
+								top: 15,
+								width: 800,
+								height: 475
 							}
 						}
 					}, {
@@ -293,7 +234,9 @@ Salg';
 						layout: {
 							style: {
 								left: 300,
-								top: 15
+								top: 15,
+								width: 390,
+								height: 210
 							}
 						}
 					}
@@ -307,8 +250,10 @@ Salg';
 						woo: 'Paper',
 						layout: {
 							style: {
-								left: 270, // FIXME offsets not working between files
-								top: 15
+								left: 290,
+								top: 15,
+								width: 400,
+								height: 150
 							}
 						}
 					},
@@ -316,8 +261,10 @@ Salg';
 						woo: 'Transformation',
 						layout: {
 							style: {
-								left: 1075,
-								top: 15
+								left: 725,
+								top: 15,
+								width: 600,
+								height: 150
 							}
 						}
 					}
@@ -362,7 +309,9 @@ Salg';
 				layout: {
 					style: {
 						left: 40,
-						top: 15
+						top: 15,
+						width: 200,
+						height: 130
 					}
 				},
 				files: files,
@@ -399,6 +348,94 @@ Salg';
 		};
 	}])
 
+	;
+
+}());
+
+(function () {
+	'use strict';
+
+	angular.module('playground.area', [ 'playground.toolbar', 'playground.browser', 'playground.transformation', 'playground.paper' ])
+
+	// TODO Show active (last clicked) widget using panel-primary and panel-info
+
+	// TODO snap areas to percentages using relative coords
+	// TODO dragable (or lockable) on/off in panel top
+
+	.directive('wooArea', ['$document', function( $document ) {
+		return {
+			restrict: 'EA',
+			scope: {
+				area: '=',
+				name: '@',
+				panelType: '@',
+				layout: '=',
+				browser: '=',
+				currentFile: '=',
+				woo: '@'
+			},
+			templateUrl: 'app/content/playground/area/area.tpl.html',
+			link: function(scope, elem) {
+				var startX = 0, startY = 0, x = 0, y = 0;
+
+				scope.dragStart = function( event ) {
+					if ( event.which === 1 ) {
+						event.preventDefault();
+						startX = event.screenX - scope.layout.style.left;
+						startY = event.screenY - scope.layout.style.top;
+						$document.on('mousemove', scope.beingDragged);
+						$document.on('mouseup', scope.dragEnd);
+					}
+				};
+
+				scope.beingDragged = function() {
+					scope.$apply( function() {
+						scope.layout.style.left = event.screenX - startX;
+						scope.layout.style.top = event.screenY - startY;
+					}	);
+				};
+
+				scope.dragEnd = function () {
+					$document.off('mousemove', scope.beingDragged);
+					$document.off('mouseup', scope.dragEnd);
+				};
+
+
+				scope.resizeStart = function( event ) {
+					if ( event.which === 1 ) {
+						event.preventDefault();
+						scope.startWidth = scope.layout.style.width;
+						scope.startHeight = scope.layout.style.height;
+						startX = event.screenX;
+						startY = event.screenY;
+						$document.on('mousemove', scope.beingResized);
+						$document.on('mouseup', scope.resizeEnd);
+					}
+				};
+
+				scope.beingResized = function() {
+					y = event.screenY - startY;
+					x = event.screenX - startX;
+
+					scope.$apply( function() {
+						scope.layout.style.width = scope.startWidth + x;
+						scope.layout.style.height = scope.startHeight + y;
+						scope.borderWidth = '3';
+					});
+				};
+
+				scope.resizeEnd = function () {
+					scope.$apply( function() {
+						scope.borderWidth = '';
+					});
+					$document.off('mousemove', scope.beingResized);
+					$document.off('mouseup', scope.resizeEnd);
+				};
+
+
+			}
+		};
+	}])
 	;
 
 }());
