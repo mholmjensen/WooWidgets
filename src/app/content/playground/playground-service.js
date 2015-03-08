d01e5557a1742c7eef0bfd28dad82f1265acadb8
Martin Holm Jensen <martin.holm.j@gmail.com>
2015-03-08 01:20:16 +0100

    Modified content in service

--- src/app/content/playground/playground-service.js
+++ src/app/content/playground/playground-service.js
@@ -8,66 +8,54 @@
 		This will either come from data storage or from default settings (as now)
 		*/
 		/*jshint multistr: true */
-		this.longRawText = 'Recipient\'s name\n\
-		Recipient\'s title\n\
-		Recipient\'s company\n\
-		Recipient\'s company address\n\
-		\n\
-		Recipient\'s Name:\n\
-		\n\
-		People read business letters quickly. Therefore, get to the point in the first paragraph--the first sentence, if possible. In other words, state what you want up front.\n\
-		\n\
-		Single space your letters and use a serif typeface. Skip a line between paragraphs. Because people read business letters quickly, use shorter sentences and paragraphs than you would in a longer document. Sentences should average fewer than twenty words, and paragraphs should average fewer than seven lines.\n\
-		\n\
-		Space your letter on the page so that it does not crowd the top. However, if possible, keep your letter to one page. Second pages often are not read. Send copies to anyone whose name you mention in the letter or who would be directly affected by the letter.\n\
-		\n\
-		Final paragraphs should tell readers what you want them to do or what you will do for them.\n\
-		\n\
-		Sincerely,\n\
-		Signature\n\
-		Name\n\
-		\n\
-		Enclosure\n\
-		\n\
-		cc: Name to receive copy';
+		this.letterOfApplication = 'Recipient\'s name\n\
+Recipient\'s title\n\
+Recipient\'s company\n\
+Recipient\'s company address\n\
+\n\
+Recipient\'s Name:\n\
+\n\
+People read business letters quickly. Therefore, get to the point in the first paragraph--the first sentence, if possible. In other words, state what you want up front.\n\
+\n\
+Single space your letters and use a serif typeface. Skip a line between paragraphs. Because people read business letters quickly, use shorter sentences and paragraphs than you would in a longer document. Sentences should average fewer than twenty words, and paragraphs should average fewer than seven lines.\n\
+\n\
+Space your letter on the page so that it does not crowd the top. However, if possible, keep your letter to one page. Second pages often are not read. Send copies to anyone whose name you mention in the letter or who would be directly affected by the letter.\n\
+\n\
+Final paragraphs should tell readers what you want them to do or what you will do for them.\n\
+\n\
+Best wishes,\n\
+\n\
+{your name}\n\
+\n\
+Enclosure\n';
 
-		this.mediumRawText = 'Recipient\'s name\n\
-		Recipient\'s title\n\
-		Recipient\'s company\n\
-		Recipient\'s company address\n\
-		\n\
-		Recipient\'s Name:\n\
-		\n\
-		People read business letters quickly. Therefore, get to the point in the first paragraph--the first sentence, if possible. In other words, state what you want up front.\n\
-		\n\
-		Single space your letters and use a serif typeface. Skip a line between paragraphs. Because people read business letters quickly, use shorter sentences and paragraphs than you would in a longer document. Sentences should average fewer than twenty words, and paragraphs should average fewer than seven lines.\n\
-		\n\
-		Sincerely,\n\
-		Signature\n\
-		Name\n\
-		\n\
-		Enclosure\n\
-		\n\
-		cc: Name to receive copy';
+		this.salesOffer = 'Recipient\'s name\n\
+Recipient\'s title\n\
+Salg';
 
-		this.shortRawText = 'Recipient\'s name\n\
-		Recipient\'s title\n\
-		Recipient\'s company\n\
-		Recipient\'s company address\n\
-		\n\
-		Recipient\'s Name:\n\
-		\n\
-		People read business letters quickly. Therefore, get to the point in the first paragraph--the first sentence, if possible. In other words, state what you want up front.\n\
-		\n\
-		Sincerely,\n\
-		Signature\n\
-		Name\n\
-		\n\
-		Enclosure\n\
-		\n\
-		cc: Name to receive copy';
 
-		this.longReplacements = [
+		this.letterOfApplicationReplacements = [
+			{
+				enabled: true,
+				from: 'Recipient\'s name',
+				to: 'Woo'
+			}, {
+				enabled: true,
+				from: 'Recipient\'s title',
+				to: 'Overwoo'
+			}, {
+				enabled: false,
+				from: 'Best',
+				to: 'The best, the best, the best, the best, the best, the best'
+			}, {
+				enabled: false,
+				from: '{your name}',
+				to: 'W'
+			}
+		];
+
+
+		this.salesOfferReplacements = [
 			{
 				enabled: true,
 				from: 'Recipient\'s name',
@@ -76,41 +64,16 @@
 				enabled: true,
 				from: 'Recipient\'s title',
 				to: 'Overlord'
-			}, {
-				enabled: false,
-				from: 'your',
-				to: 'DERES'
-			}
-		];
-
-
-		this.mediumReplacements = [
-			{
-				enabled: true,
-				from: 'Recipient\'s name',
-				to: 'Hr'
-			}, {
-				enabled: true,
-				from: 'Recipient\'s title',
-				to: 'Overlord'
-			}
-		];
-
-		this.shortReplacements = [
-			{
-				enabled: false,
-				from: 'your',
-				to: 'DERES'
 			}
 		];
 
 		var contents = [
 			{
 				paper: {
-					rawText: this.longRawText
+					rawText: this.letterOfApplication
 				},
 				transformation: {
-					replacements: this.mediumReplacements,
+					replacements: this.letterOfApplicationReplacements,
 					highlight: ''
 				},
 				candidate: {
@@ -121,10 +84,10 @@
 				}
 			}, {
 				paper: {
-					rawText: this.shortRawText
+					rawText: this.salesOffer
 				},
 				transformation: {
-					replacements: this.longReplacements,
+					replacements: this.salesOfferReplacements,
 					highlight: ''
 				},
 				candidate: {
@@ -134,19 +97,7 @@
 					}
 				}
 			}, {
-				paper: {
-					rawText: this.mediumRawText
-				},
-				transformation: {
-					replacements: this.shortReplacements,
-					highlight: ''
-				},
-				candidate: {
-					current: {
-						from: '',
-						to: ''
-					}
-				}
+
 			}
 		];
 
@@ -165,7 +116,7 @@
 						layout: {
 							style: {
 								left: 740,
-								top: 130
+								top: 15
 							}
 						}
 					}, {
@@ -173,7 +124,7 @@
 						layout: {
 							style: {
 								left: 300,
-								top: 130
+								top: 15
 							}
 						}
 					}
@@ -182,75 +133,77 @@
 				cells: []
 			},
 			{
-				cells: []
+				areas: [
+					{
+						woo: 'Paper',
+						layout: {
+							style: {
+								left: 270, // FIXME offsets not working between files
+								top: 15
+							}
+						}
+					},
+					{
+						woo: 'Transformation',
+						layout: {
+							style: {
+								left: 1075,
+								top: 15
+							}
+						}
+					}
+				]
 			},
 			{
-				areas: [{
-					woo: 'Paper',
-					layout: {
-						style: {
-							left: 240, // FIXME offsets not working between files
-							top: 130
-						}
-					}
-				}, {
-					woo: 'Transformation',
-					layout: {
-						style: {
-							left: 1060,
-							top: 130
-						}
-					}
-				}
-			]
-		}
-	];
+				cells: []
+			}
+		];
 
-	var files = [
-		{
-			name: 'F1',
-			type: 'paper',
-			woo: woos[0],
-			payload: contents[0]
-		},
-		{
-			name: 'F2',
-			type: 'logic',
-			woo: woos[1],
-			payload: contents[1]
-		},
-		{
-			name: 'F3',
-			type: 'paper',
-			woo: woos[2],
-			payload: contents[2]
-		}
-		// ,
-		// {
-		// 	name: 'F4',
-		// 	type: 'tabs',
-		// 	woo: woos[2],
-		// 	payload: contents[2]
-		// }
-	];
-
-
-	var playground = {
-		browser: {
-			layout: {
-				style: {
-					left: 100,
-					top: 130
-				}
+		var files = [
+			{
+				name: 'Letter of application',
+				type: 'paper',
+				woo: woos[0],
+				payload: contents[0]
 			},
-			files: files,
-			currentFile: files[0]
-		}
-	};
+			{
+				name: 'Sales offer',
+				type: 'paper',
+				woo: woos[1],
+				payload: contents[1]
+			},
+			{
+				name: 'Pythagoras',
+				type: 'logic',
+				woo: woos[2],
+				payload: contents[2]
+			}
+			// ,
+			// {
+			// 	name: 'F4',
+			// 	type: 'tabs',
+			// 	woo: woos[2],
+			// 	payload: contents[2]
+			// }
+		];
 
-	return playground;
 
-})
-;
+		var playground = {
+			browser: {
+				layout: {
+					style: {
+						left: 40,
+						top: 15
+					}
+				},
+				files: files,
+				currentFile: files[0]
+			}
+		};
+
+		return playground;
+
+	})
+	;
 
 }());
