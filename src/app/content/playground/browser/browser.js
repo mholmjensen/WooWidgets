c943610be196f83e5ca30ef2ab2e7336da04976f
Martin Holm Jensen <martin.holm.j@gmail.com>
2015-03-09 02:59:21 +0100

    Rename feature deferred for now

--- src/app/content/playground/browser/browser.js
+++ src/app/content/playground/browser/browser.js
@@ -11,17 +11,27 @@
 			},
       templateUrl: 'app/content/playground/browser/browser.tpl.html',
 			link: function( scope ) {
+				scope.editingName = false;
 				scope.fileSelected = function( event, file ) {
-					scope.browser.currentFile = file;
-					scope.selectedFile = file;
+					// Not very spectacular stuff this here
+					var candidate = file;
+					angular.forEach( scope.browser.files, function( otherFile, key ) {
+						if ( file.name === otherFile.name ) { //FIXME: Messes up on same names, and generally risky unless in tree
+							candidate = otherFile;
+							console.log('found' + file.name );
+							return false;
+						}
+					});
+					scope.browser.currentFile = candidate;
 				};
 				scope.fileMoved = function( event, index, file ) {
 					var remfile = scope.browser.files.splice( index, 1 );
-					scope.browser.currentFile = file;
-					scope.selectedFile = file;
+					scope.fileSelected( event, remfile[0] );
 				};
 			}
     };
   })
+
+	;
 
 }());
