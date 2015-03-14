252295a1e59e02e1d205658f2cda12d73f99a32c
Martin Holm Jensen <martin.holm.j@gmail.com>
2015-03-14 04:27:11 +0100

    Added debug inspector of scope

--- src/app/content/playground/debug/inspector/inspector.js
+++ src/app/content/playground/debug/inspector/inspector.js
@@ -4,26 +4,31 @@
 	angular.module('playground.debug.inspector', [ ])
 
 	// Inherits scope of parent (TODO read up on scope: true)
-	.directive('wooInspector', function( ) {
+	.directive('wooDebugInspector', function( $log ) {
 		return {
 			restrict: 'EA',
-			scope: true,
 			// require: '^wooDebug',
 			templateUrl: 'app/content/playground/debug/inspector/inspector.tpl.html',
-			link: function( scope, elem, $attr ) {
-				scope.onTop = angular.isDefined( $attr.onTop ); // FIXME: Introduce
+			link: function( scope, elem, attrs ) {
+				attrs.$addClass( 'woo-debug-inspector' );
+				if ( angular.isDefined( attrs.$attr.inline ) ) { // TODO: Should be set directly as optional css class? bootstrap style?
+					attrs.$addClass( 'inspector added inline' );
+				}
+
 			}
 		};
 	})
 
-	.directive('wooInspectorToggle', function( ) {
+	.directive('wooDebugInspectorToggle', function( $log ) {
 		return {
 			restrict: 'EA',
-			scope: true,
 			templateUrl: 'app/content/playground/debug/inspector/inspector-toggle.tpl.html',
-			link: function( scope, elem, $attr ) {
-				scope.onTop = angular.isDefined( $attr.onTop );
-				console.log(scope.onTop);
+			link: function( scope, elem, attrs ) {
+				attrs.$addClass( 'woo-debug-inspector-toggle' );
+				if ( angular.isDefined( attrs.$attr.inline ) ) {
+					$log.log('toggle added inline');
+					attrs.$addClass( 'inline' );
+				}
 			}
 		};
 	})
